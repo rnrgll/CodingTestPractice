58ad7f771d5eac0ba67b350bdd5760c8c8bdf4f2
rnrgll <shuajjjjj@naver.com>
2025-03-14 19:31:25 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 2.01 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
@@ -3,30 +3,8 @@ using System.Collections.Generic;
 
 public class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        
-        List<int> minusVals = new List<int>();
-        List<int> plusVals = new List<int>();
-        
-        for(int i=0; i<numbers.Length; i++){
-            if(numbers[i]<0) minusVals.Add(numbers[i]);
-            else plusVals.Add(numbers[i]);
-        }
-        
-        minusVals.Sort();
-        plusVals.Sort();
-        
-        int maxVal1 = int.MinValue;
-        int maxVal2 = maxVal1;
-        
-        if(minusVals.Count==1 && plusVals.Count==1) answer = minusVals[0] * plusVals[0];
-        else{
-            if(minusVals.Count > 1) maxVal1 = minusVals[0] * minusVals[1];
-            if(plusVals.Count>1) maxVal2 = plusVals[plusVals.Count-1] * plusVals[plusVals.Count-2];
-            answer = Math.Max(maxVal1, maxVal2);
-        }
-        
-        return answer;
-        
+        Array.Sort(numbers);
+        int result = Math.Max(numbers[0]*numbers[1], numbers[numbers.Length-1]*numbers[numbers.Length-2]);
+        return result;
     }
 }
